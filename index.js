ef1828758a570fa4b5fcc00b4157c36f1ebe82e4
gantalaavinash <gantalaavinash@gmail.com>
2021-04-05 17:13:48 +0530

    Portfolio

---
+++ index.js
@@ -0,0 +1,17 @@
+function whoweare() {
+    var x = document.getElementById("whoweare");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function technologiles() {
+  var x = document.getElementById("Technologiles");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
